a39c64ea2507c745315b23fc97884788d24b1ee3
equal-l2 <eng.equall2@gmail.com>
2023-05-14 00:08:46 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from io import BytesIO
+from random import randint, random
+
+import cv2
+import numpy as np
+import tensorflow as tf
+from nsfw_detector import predict
+
+print("Loading model")
+model = predict.load_model("./model/mobilenet_v2_140_224")
+print("Ended: Loading model")
+
+IMG_SHAPE = (250, 200, 3)
+
+max_value = 0
+while True:
+    # create image
+    # TODO: genetic algorithm
+    image = np.zeros(IMG_SHAPE, np.uint8)
+    for _ in range(1000):
+        x = randint(0, IMG_SHAPE[1])
+        w = randint(x, IMG_SHAPE[1]) - x
+        y = randint(0, IMG_SHAPE[0])
+        h = randint(y, IMG_SHAPE[0]) - y
+        color = tuple(randint(0, 255) for _ in range(3))
+        # print(y,h,x,w)
+        rect = np.full((h, w, 3), color, np.uint8)
+
+        # 重なった部分だけを上手いこと透過させたいのでクロップしてからweighted sum
+        # https://stackoverflow.com/a/56472613
+        opacity = random()
+        sub_img = image[y : y + h, x : x + w]
+        # print(rect.shape, sub_img.shape)
+        cv2.addWeighted(sub_img, opacity, rect, 1 - opacity, 0, dst=sub_img)
+        image[y : y + h, x : x + w] = sub_img
+
+    # なんか一旦ファイルに落とさないとまともに動かないのでごまかす
+    # https://github.com/GantMan/nsfw_model/issues/89#issuecomment-770139533
+    # https://stackoverflow.com/a/52865864
+    success, cv2_buf = cv2.imencode(".png", image)
+    bytes_buf = BytesIO(cv2_buf)
+
+    # モデルが食える形にしてやる
+    # TODO: BytesIO使わなくてもいけるのでは……(リサイズと正規化できればいい気がする)
+    tf_img = tf.keras.preprocessing.image.load_img(bytes_buf, target_size=(224, 224))
+    tf_image = tf.keras.preprocessing.image.img_to_array(tf_img) / 255.0
+
+    result = predict.classify_nd(
+        model,
+        np.asarray([tf_image]),
+        # プログレスバーを消す
+        predict_args={
+            "verbose": 0,
+        },
+    )
+
+    value = result[0]["hentai"]
+
+    if value > max_value:
+        print("new value:", value)
+        max_value = value
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
